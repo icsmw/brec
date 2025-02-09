18e77a3455fb9d2051d4dc5a0d72f372e6146cab
DmitryAstafyev <dvastafyev@gmail.com>
2025-02-10 00:48:58 +0100

    Add WriteVectoredTo

--- brec/src/traits/write.rs
+++ brec/src/traits/write.rs
@@ -2,3 +2,52 @@ pub trait WriteTo {
     fn write<T: std::io::Write>(&self, buf: &mut T) -> std::io::Result<usize>;
     fn write_all<T: std::io::Write>(&self, buf: &mut T) -> std::io::Result<()>;
 }
+
+pub trait WriteVectoredTo {
+    fn write_vectored<T: std::io::Write>(&self, buf: &mut T) -> std::io::Result<usize> {
+        buf.write_vectored(&self.slices()?[..])
+    }
+
+    fn slices(&self) -> std::io::Result<Vec<std::io::IoSlice<'_>>>;
+
+    fn write_vectored_all<T: std::io::Write>(&self, buf: &mut T) -> std::io::Result<()> {
+        let source = self.slices()?;
+        let mut written = 0;
+
+        loop {
+            let mut offset = 0;
+            let in_work = source
+                .iter()
+                .filter_map(|slice| {
+                    let start = offset;
+                    let end = offset + slice.len();
+                    offset = end;
+
+                    if written >= end {
+                        None
+                    } else if written <= start {
+                        Some(*slice)
+                    } else {
+                        let consumed = written - start;
+                        Some(std::io::IoSlice::new(&slice[consumed..]))
+                    }
+                })
+                .collect::<Vec<_>>();
+
+            if in_work.is_empty() {
+                break;
+            }
+
+            let just_written = buf.write_vectored(&in_work)?;
+            written += just_written;
+
+            if just_written == 0 {
+                return Err(std::io::Error::new(
+                    std::io::ErrorKind::WriteZero,
+                    "Failed to write data",
+                ));
+            }
+        }
+        Ok(())
+    }
+}
