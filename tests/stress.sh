62f718923d28ed812966134ce38c54920ef0a67b
DmitryAstafyev <dvastafyev@gmail.com>
2026-03-15 20:27:36 +0100

    test(brec): add test execution for resilient feature (stress)

--- tests/stress.sh
+++ tests/stress.sh
@@ -9,14 +9,15 @@ cd ./stress_payloads
 sh ./stress.sh
 cd ..
 
-# cd ./stress_payloads_crypt
-# sh ./stress.sh
-# cd ..
+cd ./stress_payloads_crypt
+sh ./stress.sh
+cd ..
+
+cd ./stress_resilient
+sh ./stress.sh
+cd ..
 
 cd ./stress_packets
 sh ./stress.sh
 cd ..
 
-cd ./measurements
-cargo test  --release -- --nocapture
-cd ..
