6887c1031f1352bc328664368a1fc62d98b31357
DmitryAstafyev <dvastafyev@gmail.com>
2026-04-23 23:11:56 +0200

    refactor(all): optimize bash scripts

--- generator/lint.sh
+++ generator/lint.sh
@@ -5,8 +5,13 @@ set -euo pipefail
 SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 cd "$SCRIPT_DIR"
 
+excluded_paths=(
+    "gen_tests"
+)
+
 for integration in $(ls -1); do
     [ -d "$integration" ] || continue
+    [[ " ${excluded_paths[*]} " == *" $integration "* ]] && continue
 
     for crate in $(ls -1 "$integration"); do
         path="$integration/$crate"
